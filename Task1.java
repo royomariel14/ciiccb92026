8b1c768a522c74df6078e122a460264e0ab1ff4f
Mariel Royo <royomariel14@gmail.com>
2026-07-15 16:55:56 +0800

    Task 1 completed

---
+++ Task1.java
@@ -0,0 +1,11 @@
+
+public class Task1 {
+    /** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); // Display the string.
+    }
+}
+
