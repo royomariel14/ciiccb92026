bf2e88f57fbec74ac2a5fbef1d3aa152c8b39a11
royomariel14 <royomariel14@gmail.com>
2026-07-23 07:57:58 +0000

    Done Task2

--- Task2.java
+++ Task2.java
@@ -1,1 +1,15 @@
+public class Task2{
+    public static void main(String[] args) {
+    
+    byte zero = 0;
+    short three = 3;
+    int two = 2;
+    float decimal = 2.0f;
+    char h = 'H';
+    boolean truth = true;
 
+    String output = "" + h + three + "ll" + zero + " w" + zero + "rld " + decimal + " " + truth;
+
+    System.out.println(output);
+}
+}
